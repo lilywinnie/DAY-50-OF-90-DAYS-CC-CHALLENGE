fc34dab97eba8a83912bc4b8ba3b7f3d24523b86
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-08-13 23:39:05 +0600

    Create ans3.cpp

---
+++ ans3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int T;
+    cin>>T;
+    
+    while(T--)
+    {
+        int M,S;
+        cin>>M>>S;
+        
+        cout<<M/S<<"\n";
+    }
+}
